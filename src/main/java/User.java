66764a7d227e6d906de181b61eace2ae01a66322
Félicien Clarté <clartefelicien@hotmail.com>
2016-06-01 15:23:39 +0200

    modification

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -25,14 +25,30 @@ public class User {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-        User user = (User) o;
 
-        return id.equals(user.id);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
     @Override
     public String toString() {
